016e0177fdc6d87ca233fa2952d30c3d39b59052
max.jian <max.jian@co-rock.com>
2020-09-12 21:24:32 +0800

    Initial Yagi project

---
+++ Sources/Yagi.h
@@ -0,0 +1,19 @@
+//
+//  Yagi.h
+//  Yagi
+//
+//  Created by max on 2020/9/12.
+//  Copyright © 2020 max. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Yagi.
+FOUNDATION_EXPORT double YagiVersionNumber;
+
+//! Project version string for Yagi.
+FOUNDATION_EXPORT const unsigned char YagiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yagi/PublicHeader.h>
+
+
